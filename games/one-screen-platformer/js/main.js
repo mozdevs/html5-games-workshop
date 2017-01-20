0518e9988c46033a8c10e04bf7985b9c05f4b018
Belén Albeza <belen.albeza@gmail.com>
2017-01-20 13:35:00 +0100

    add animations for main character

--- games/one-screen-platformer/js/main.js
+++ games/one-screen-platformer/js/main.js
@@ -10,10 +10,17 @@ function Hero(game, x, y) {
     // call Phaser.Sprite constructor
     Phaser.Sprite.call(this, game, x, y, 'hero');
 
+    // anchor
     this.anchor.set(0.5, 0.5);
-
     // physics properties
     this.game.physics.enable(this); this.body.collideWorldBounds = true;
+    // animations
+    this.animations.add('stop', [0]);
+    this.animations.add('run', [1, 2], 8, true); // 8fps looped
+    this.animations.add('jump', [3]);
+    this.animations.add('fall', [4]);
+    // starting animation
+    this.animations.play('stop');
 }
 
 // inherit from Phaser.Sprite
@@ -23,6 +30,14 @@ Hero.prototype.constructor = Hero;
 Hero.prototype.move = function (direction) {
     const SPEED = 200;
     this.body.velocity.x = direction * SPEED;
+
+    // update image flipping & animations
+    if (this.body.velocity.x < 0) {
+        this.scale.x = -1;
+    }
+    else if (this.body.velocity.x > 0) {
+        this.scale.x = 1;
+    }
 };
 
 Hero.prototype.jump = function () {
@@ -31,8 +46,37 @@ Hero.prototype.jump = function () {
     let canJump = this.body.touching.down;
     if (canJump) {
         this.body.velocity.y = -JUMP_SPEED;
+        this.isJumping = true;
     }
     return canJump;
+};
+
+Hero.prototype.update = function () {
+    // update sprite animation, if it needs changing
+    let animationName = this._getAnimationName();
+    if (this.animations.name !== animationName) {
+        this.animations.play(animationName);
+    }
+};
+
+// returns the animation name that should be playing depending on
+// current circumstances
+Hero.prototype._getAnimationName = function () {
+    let name = 'stop'; // default animation
+
+    // jumping
+    if (this.body.velocity.y < 0) {
+        name = 'jump';
+    }
+    // falling
+    else if (this.body.velocity.y >= 0 && !this.body.touching.down) {
+        name = 'fall';
+    }
+    else if (this.body.velocity.x !== 0 && this.body.touching.down) {
+        name = 'run';
+    }
+
+    return name;
 };
 
 // =============================================================================
@@ -92,7 +136,7 @@ PlayState.preload = function () {
     this.game.load.image('grass:2x1', 'images/grass_2x1.png');
     this.game.load.image('grass:1x1', 'images/grass_1x1.png');
 
-    this.game.load.image('hero', 'images/hero_stopped.png');
+    this.game.load.spritesheet('hero', 'images/hero.png', 36, 42);
     this.game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
 
     this.game.load.audio('sfx:jump', 'audio/jump.wav');
@@ -165,8 +209,8 @@ PlayState._loadLevel = function (data) {
         sprite.body.allowGravity = false;
         sprite.body.immovable = true;
         // animations
-        sprite.animations.add('rotate', [0, 1, 2, 1]);
-        sprite.animations.play('rotate', 6, true);
+        sprite.animations.add('rotate', [0, 1, 2, 1], 6, true); // 6fps, looped
+        sprite.animations.play('rotate');
     }.bind(this));
 
     // enable gravity
