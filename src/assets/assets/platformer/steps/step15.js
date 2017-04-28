9d3b9a36eb5b3cc7041d73ed08031f14287d6502
Yoan Blanc <yoan@dosimple.ch>
2017-04-28 10:05:45 +0200

    Flip the spider sprite when it turns around.
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- src/assets/assets/platformer/steps/step15.js
+++ src/assets/assets/platformer/steps/step15.js
@@ -85,7 +85,7 @@ function Spider(game, x, y) {
     Phaser.Sprite.call(this, game, x, y, 'spider');
 
     // anchor
-    this.anchor.set(0.5);
+    this.anchor.set(0.5, 0.5);
     // animation
     this.animations.add('crawl', [0, 1, 2], 8, true); // 8fps, looped
     this.animations.add('die', [0, 4, 0, 4, 0, 4, 3, 3, 3, 3, 3, 3], 12);
@@ -94,22 +94,23 @@ function Spider(game, x, y) {
     // physic properties
     this.game.physics.enable(this);
     this.body.collideWorldBounds = true;
-    this.body.velocity.x = Spider.SPEED;
+    this.body.velocity.x = -this.SPEED;
 }
-
-Spider.SPEED = 100;
 
 // inherit from Phaser.Sprite
 Spider.prototype = Object.create(Phaser.Sprite.prototype);
 Spider.prototype.constructor = Spider;
+Spider.prototype.SPEED = 100;
 
 Spider.prototype.update = function () {
     // check against walls and reverse direction if necessary
     if (this.body.touching.right || this.body.blocked.right) {
         this.body.velocity.x = -Spider.SPEED; // turn left
+        this.scale.x *= -1; // flip the sprite
     }
     else if (this.body.touching.left || this.body.blocked.left) {
         this.body.velocity.x = Spider.SPEED; // turn right
+        this.scale.x *= -1; // flip the sprite
     }
 };
 
